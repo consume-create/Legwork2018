a827734f182ad9d14a8094d8835939cf81ce8055
Drew Dahlman <info@drewdahlman.com>
2017-10-27 15:03:23 -0600

    percentage to the footer

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -1,12 +1,18 @@
 <template>
   <footer>
     <span>Up Next ...</span>
+    <div class='cover'></div>
   </footer>
 </template>
 
 <script>
 export default {
   name: 'footer-view',
+  data: function(){
+    return {
+      percent: 0
+    }
+  },
   methods: {
     /*
     ------------------------------------------
@@ -18,9 +24,16 @@ export default {
     ------------------------------------------ */
     onScroll(e) {
       window.requestAnimationFrame(() => {
-        if( Math.floor(this._$footer.offset().top - $(window).scrollTop()) <= 0 ){
+        let offset = Math.floor(this._$footer.offset().top - $(window).scrollTop());
+
+        if( offset < this._$footer.height() ){
+          this.percent = (offset / this._$footer.height()) * 100;
+        }
+        if( offset <= 0 ){
           this.$router.push( '/' + this.$store.getters.nextPage );
         }
+
+        this._$cover.css('transform', `translate(-${this.percent}%, 0)`);
       });
     }
   },
@@ -35,6 +48,7 @@ export default {
     // class vars
     this._$wn = $(window);
     this._$footer = $(this.$el);
+    this._$cover = $(".cover", this._$footer);
 
     // events
     this._$wn
@@ -70,4 +84,12 @@ footer
     top: 50%
     left: 50%
     transform: translate(-50%, -50%)
+
+  .cover
+    position: absolute
+    height: 100%
+    width: 100%
+    transform: translate(-100%, 0px)
+    background:
+      color: $color-bg-yellow
 </style>
