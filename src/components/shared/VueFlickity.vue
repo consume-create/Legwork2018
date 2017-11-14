3ff16682398cfd4b9acbf08a4b64c222ed2c9cac
Drew Dahlman <info@drewdahlman.com>
2017-11-14 14:54:24 -0700

    updating VueFlickity and adding flickity to package.json

--- src/components/shared/VueFlickity.vue
+++ src/components/shared/VueFlickity.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-// import Flickity from 'flickity';
 
 export default {
     name: 'vue-flickity',
@@ -21,7 +20,7 @@ export default {
     },
 
     mounted () {
-      // this.init();
+      this.init();
     },
 
     beforeDestroy () {
@@ -30,103 +29,104 @@ export default {
     },
 
     methods: {
-        // init() {
-        //     // this.flickity = new Flickity(this.$el, this.options);
-        //     this.$emit('init', this.flickity);
-        // },
+        init() {
+            let Flickity = require('flickity');
+            this.flickity = new Flickity(this.$el, this.options);
+            this.$emit('init', this.flickity);
+        },
 
-        // next (isWrapped, isInstant) {
-        //     this.flickity.next(isWrapped, isInstant);
-        // },
+        next (isWrapped, isInstant) {
+            this.flickity.next(isWrapped, isInstant);
+        },
 
-        // previous (isWrapped, isInstant) {
-        //     this.flickity.previous(isWrapped, isInstant);
-        // },
+        previous (isWrapped, isInstant) {
+            this.flickity.previous(isWrapped, isInstant);
+        },
 
-        // select (index, isWrapped, isInstant) {
-        //     this.flickity.select(index, isWrapped, isInstant);
-        // },
+        select (index, isWrapped, isInstant) {
+            this.flickity.select(index, isWrapped, isInstant);
+        },
 
-        // selectedIndex () {
-        //     return this.flickity.selectedIndex
-        // },
+        selectedIndex () {
+            return this.flickity.selectedIndex
+        },
 
-        // selectCell (value, isWrapped, isInstant) {
-        //     this.flickity.selectCell( value, isWrapped, isInstant );
-        // },
+        selectCell (value, isWrapped, isInstant) {
+            this.flickity.selectCell( value, isWrapped, isInstant );
+        },
 
-        // resize () {
-        //     this.flickity.resize();
-        // },
+        resize () {
+            this.flickity.resize();
+        },
 
-        // reposition () {
-        //     this.flickity.reposition();
-        // },
+        reposition () {
+            this.flickity.reposition();
+        },
 
-        // prepend (elements) {
-        //     this.flickity.prepend(elements);
-        // },
+        prepend (elements) {
+            this.flickity.prepend(elements);
+        },
 
-        // append (elements) {
-        //     this.flickity.append(elements);
-        // },
+        append (elements) {
+            this.flickity.append(elements);
+        },
 
-        // insert (elements, index) {
-        //     this.flickity.insert(elements, index);
-        // },
+        insert (elements, index) {
+            this.flickity.insert(elements, index);
+        },
 
-        // remove (elements) {
-        //     this.flickity.remove(elements);
-        // },
+        remove (elements) {
+            this.flickity.remove(elements);
+        },
 
-        // playPlayer () {
-        //     this.flickity.playPlayer();
-        // },
+        playPlayer () {
+            this.flickity.playPlayer();
+        },
 
-        // stopPlayer () {
-        //     this.flickity.stopPlayer();
-        // },
+        stopPlayer () {
+            this.flickity.stopPlayer();
+        },
 
-        // pausePlayer () {
-        //     this.flickity.pausePlayer();
-        // },
+        pausePlayer () {
+            this.flickity.pausePlayer();
+        },
 
-        // unpausePlayer () {
-        //     this.flickity.unpausePlayer();
-        // },
+        unpausePlayer () {
+            this.flickity.unpausePlayer();
+        },
 
-        // rerender () {
-        //     this.flickity.destroy();
-        //     this.init();
-        // },
+        rerender () {
+            this.flickity.destroy();
+            this.init();
+        },
 
-        // destroy () {
-        //     this.flickity.destroy();
-        // },
+        destroy () {
+            this.flickity.destroy();
+        },
 
-        // reloadCells () {
-        //     this.flickity.reloadCells();
-        // },
+        reloadCells () {
+            this.flickity.reloadCells();
+        },
 
-        // getCellElements () {
-        //     this.flickity.getCellElements();
-        // },
+        getCellElements () {
+            this.flickity.getCellElements();
+        },
 
-        // data () {
-        //     return Flickity.data(this.$el);
-        // },
+        data () {
+            return Flickity.data(this.$el);
+        },
 
-        // on (eventName, listener) {
-        //     this.flickity.on(eventName, listener);
-        // },
+        on (eventName, listener) {
+            this.flickity.on(eventName, listener);
+        },
 
-        // off (eventName, listener) {
-        //     this.flickity.off(eventName, listener);
-        // },
+        off (eventName, listener) {
+            this.flickity.off(eventName, listener);
+        },
 
-        // once (eventName, listener) {
-        //     this.flickity.once(eventName, listener);
-        // }
+        once (eventName, listener) {
+            this.flickity.once(eventName, listener);
+        }
     },
 
 };
