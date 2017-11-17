a8f673d2087e73eb168ad8efb677ed3e931bacc1
Drew Dahlman <info@drewdahlman.com>
2017-11-17 12:58:35 -0700

    adding video component

---
+++ src/components/shared/VideoComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="video">
+    <!-- if video-id use vimdeo video embed -->
+    <div class="hero-image"></div>
+    <no-ssr>
+      <vimeo-player :video-id='124884771'></vimeo-player>
+    </no-ssr>
+    <!-- else load up an image -->
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import NoSSR from 'vue-no-ssr';
+
+export default {
+	name: 'video-component',
+  components: {
+    'no-ssr': NoSSR
+  },
+	/*
+	------------------------------------------
+	| props:void (-)
+	|
+	| hero:object
+	------------------------------------------ */
+	props: [],
+  data: function() {
+    return {
+      'hero_type': '',
+      'hero_image': '',
+      'vimeo_id': '',
+    }
+  }
+}
+</script>
+
+<style lang='sass' scoped>
+@import "src/styles/global"
+</style>
