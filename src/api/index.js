0b7599f92f78cbeee8c8aebc57a53f7fa69fcb98
Matt Wiggins <matt.wiggins@legworkstudio.com>
2018-05-07 15:25:35 -0600

    Hook the Wordpress junk back up.

--- src/api/index.js
+++ src/api/index.js
@@ -11,9 +11,11 @@ const writer = new streams.WritableStream();
 ------------------------------------------ */
 export function init(){
   if( writer.toString() == '' || process.env.NODE_ENV == 'development'){
+    let _url = process.env.NODE_ENV == 'development' ? 'staging_project.json' : 'project.json';
+
     return new Promise( (resolve, reject) => {
       request({
-        url: 'http://localhost:8080/data/site.json',
+        url: 'http://assets.legwork.studio/data/' + _url,
         json: true
       }, function (error, response, body) {
         if (!error && response.statusCode === 200) {
