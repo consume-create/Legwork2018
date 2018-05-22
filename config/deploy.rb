408372815df1fe33e79fdfac65871082bca59cc8
Dan Hannigan <danjhannigan@gmail.com>
2018-05-22 10:15:25 -0600

    Removing version lock because latest cap is higher

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.10.0"
+# lock "3.10.0"
 
 set :application, 'legwork2018'
 set :repo_url, 'git@github.com:legworkstudio/Legwork2018.git'
