044a2530086d5a9f811ac961a41da91ffcf5edb9
Matt Wiggins <matt.wiggins@legworkstudio.com>
2018-05-24 17:58:31 -0600

    Biz widget fade out and notes for finalizing melt mode.

--- src/views/BizWidgetView.vue
+++ src/views/BizWidgetView.vue
@@ -3,7 +3,7 @@
     <div id="biz-inner" @scroll="onBizWidgetScroll">
       <studio-about></studio-about>
     </div>
-    <div id="arbitrary-thing"></div>
+    <div id="biz-widget-mask"></div>
   </div>
 </template>
 
@@ -28,19 +28,18 @@ export default {
       let pos = typeof this.$route.query.slide === 'undefined' ? '' : this.$route.query.slide;
       this.$store.dispatch('SET_BIZ_POSITION', { pos: pos, delay: 0 });
 
-      // test
+      // mask
+      // TODO: other event namespaces
       if(pos === '') {
-        this.updateMask(0);
+        setTimeout(() => this.updateMask(0), 333);
       } else {
-        let iteration = 0;
-        this._$arbitrary_thing.off('webkitAnimationStart').on('webkitAnimationStart', (e) => {
-          iteration += 1;
-        });
-        this._$arbitrary_thing.off('webkitAnimationIteration').on('webkitAnimationIteration', (e) => {
+        let iteration = 1;
+        this._$biz_widget_mask.off('webkitAnimationIteration').on('webkitAnimationIteration', (e) => {
           this.updateMask(iteration);
           iteration += 1;
         });
-        this._$arbitrary_thing.off('webkitAnimationEnd').on('webkitAnimationEnd', (e) => {
+        this._$biz_widget_mask.off('webkitAnimationEnd').on('webkitAnimationEnd', (e) => {
+          // TODO: turn mask off?
           this.updateMask(30);
         });
       }
@@ -74,7 +73,7 @@ export default {
   mounted() {
     // class vars
     this._$biz_widget = $('#biz-widget');
-    this._$arbitrary_thing = $('#arbitrary-thing');
+    this._$biz_widget_mask = $('#biz-widget-mask');
   },
   ssrInit ({ store, route }) {
     // biz position
@@ -95,48 +94,20 @@ export default {
   height: 100%
   color: $grandpas-office
   overflow: hidden
-  visibility: hidden
-  //transition: visibility 0ms linear $speed-metal
-
-  //mask: url(/svgs/masks.svg#mask)
-
   mask: url(/images/cool-tear-mask.png)
   mask-repeat: no-repeat
   mask-size: 100% 3100%
-  mask-position: 0% 0%
-
-  // &:before
-  //   content: ""
-  //   position: absolute
-  //   top: 50%
-  //   left: 50%
-  //   width: 100%
-  //   height: 100%
-  //   transform: translate3d(-50%, -50%, 0)
-  //   background-image: url(/images/shitty-erase-2.png)
-  //   background-repeat: no-repeat
-  //   background-size: 100% 2000%
-  //   background-position-y: 0%
-  //   z-index: -1
+  opacity: 0
+  visibility: hidden
+  transition: opacity $fast linear 0ms, visibility 0ms linear $fast
 
   &.wide
-    //transition: visibility 0ms linear 0ms
+    transition: opacity 0ms linear 0ms, visibility 0ms linear 0ms
+    opacity: 1
     visibility: visible
-    //animation: mask-in 1.5s steps(19) normal forwards
 
-    // &:before
-    //   animation: erase-page 1.5s steps(19) normal forwards
-
-    // #biz-inner
-    //   transform: translate3d(0%, 0px, 0)
-    //   transition: transform $speed-metal $evil-ease $speed-metal
-
-    // #biz-inner
-    //   opacity: 1
-    //   transition: opacity $speed-metal $evil-ease $speed-metal
-
-    #arbitrary-thing
-      animation: test 66ms linear 30
+    #biz-widget-mask
+      animation: mask-proxy 75ms linear 30
 
   #biz-inner
     box-sizing: border-box
@@ -146,23 +117,14 @@ export default {
     background-color: $darkerGray
     overflow: scroll
 
-    // transform: translate3d(100%, 0px, 0)
-    // transition: transform $speed-metal $evil-ease 0ms
-
-    // opacity: 0
-    // transition: opacity $speed-metal $evil-ease 0ms
-
-    h2
-      color: $grandpas-office
-
-  #arbitrary-thing
+  #biz-widget-mask
     position: fixed
     top: -100px
     left: 0px
     visibility: hidden
     transform: translate3d(0px, 0px, 0)
 
-@keyframes test
+@keyframes mask-proxy
   100%
     transform: translate3d(1px, 0px, 0)
 </style>
