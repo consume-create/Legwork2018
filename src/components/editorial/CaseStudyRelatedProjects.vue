b456db829952577ae7532d16359c13417d6457de
Dan Hannigan <danjhannigan@gmail.com>
2018-06-14 16:39:30 -0600

    Tightening up tne next project area

--- src/components/editorial/CaseStudyRelatedProjects.vue
+++ src/components/editorial/CaseStudyRelatedProjects.vue
@@ -2,11 +2,11 @@
   <div class='case-study-related-projects'>
     <div class="next-project-image"></div>
     <div class="project-header">Next Project</div>
-    <router-link :to="project.next_project.post_name" >
-      <span class="big-type">{{project.next_project.post_title}}</span>
+    <router-link :to="project.next_project.post_name" class="next-project-link">
+      <span class="next-project-link big-type">{{project.next_project.post_title}}</span>
     </router-link>
-    <router-link :to="project.type" >
-      <div class="gallery-link">Back to Animation</div>
+    <router-link :to="project.type" class="gallery-link">
+      <div>Back to Animation</div>
     </router-link>
   </div>
 </template>
@@ -47,15 +47,29 @@ export default {
   a
     color: $white
     font-weight: 700
-    grid-column: 3 / span 16
-    line-height: 80px
-    margin-bottom: span(2, 20)
-    max-width: 500px
-    padding-bottom: 32px
     text-decoration: none
+
+  .next-project-link
+    font-size: 24px
+    grid-column: 3 / span 16
+    line-height: 40px
+    margin-bottom: span(2, 20)
+    padding-bottom: 32px
 
   .gallery-link
     grid-column: 3 / span 16
     opacity: 0.5
 
++respond-to($tablet-landscape)
+  .case-study-related-projects
+    .project-header
+      font-size: 24px
+      font-weight: 700
+      grid-column: 3 / span 16
+
+    .next-project-link
+      font-size: 60px
+      grid-column: 3 / span 9
+      line-height: 80px
+      padding-bottom: 32px
 </style>
