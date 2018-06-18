e21dcf05593ec22634d568d6745f0deb2fed1d46
Dan Hannigan <danjhannigan@gmail.com>
2018-06-18 15:03:56 -0600

    Rough static stub on Search

---
+++ src/components/PowerlistOverlay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="powerlist-overlay">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'powerlist-overlay',
+
+  /*
+  ------------------------------------------
+  | props:void (-)
+  |
+  | Project:object
+  ------------------------------------------ */
+  props: [
+  ],
+}
+</script>
+
+<style lang='sass'>
+@import "src/styles/global"
+
+#powerlist-overlay
+  width: 100%
+  height: 100%
+  background: $color-bg-body
+  text: $color-text
+
+</style>
---
+++ src/components/WatchOverlay.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="watch-overlay">
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'watch-overlay',
+
+  /*
+  ------------------------------------------
+  | props:void (-)
+  |
+  | Project:object
+  ------------------------------------------ */
+  props: [
+  ],
+}
+</script>
+
+<style lang='sass'>
+@import "src/styles/global"
+
+#watch-overlay
+  width: 100%
+  height: 100%
+  background: $black
+  text: $faded
+
+</style>
