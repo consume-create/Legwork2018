167dbcd9b3e9e29f42c52ac3bed789b1d9692217
jejacks0n <jejacks0n@gmail.com>
2018-12-05 10:46:01 -0700

    removes remote loading of data.json and puts it in /public

--- src/api/index.js
+++ src/api/index.js
@@ -1,35 +1,24 @@
-import request from 'request';
-const streams = require('memory-streams');
-
-const writer = new streams.WritableStream();
+const fs = require('fs')
+const streams = require('memory-streams')
+const writer = new streams.WritableStream()
 
 /*
-------------------------------------------
-| init:void (-)
-|
-| Method Notes
------------------------------------------- */
-export function init(){
-  if( writer.toString() == '' || process.env.NODE_ENV == 'development'){
-    let _url = process.env.NODE_ENV == 'development' ? 'staging_project.json' : 'production_project.json';
-
-    return new Promise( (resolve, reject) => {
-      request({
-        url: 'http://d8pghywiq8stv.cloudfront.net/data/' + _url,
-        json: true
-      }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-          writer.write(JSON.stringify(body));
-          resolve(body);
-        } else {
-          reject(error);
-        }
-      });
-    });
+ ------------------------------------------
+ | init:void (-)
+ |
+ | Method Notes
+ ------------------------------------------ */
+export function init() {
+  if (writer.toString() === '' || process.env.NODE_ENV === 'development') {
+    return new Promise((resolve) => {
+      let rawdata = fs.readFileSync('public/data.json')
+      writer.write(rawdata)
+      resolve(JSON.parse(rawdata))
+    })
   } else {
-    return new Promise( (resolve) => {
-      const cache = JSON.parse(writer.toString());
-      resolve(cache);
-    });
+    return new Promise((resolve) => {
+      const cache = JSON.parse(writer.toString())
+      resolve(cache)
+    })
   }
 }
