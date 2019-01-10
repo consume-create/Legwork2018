61cc715ab27a22b73b4c31a07cbbd1a172dec232
Lucas Drummond <ldrummond@wustl.edu>
2019-01-10 17:59:50 -0600

    Schema base modules that will reflect media approach

---
+++ src/components/schema-base.js
@@ -0,0 +1,48 @@
+/*
+*
+* Schema Base 
+* - defines base schema components to be used. 
+*
+*/
+
+export default schemaBase = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type":"object",
+  "definitions": {
+    "image": {
+      "title" : "Image",
+      "type": "object",
+      "properties": {
+        "filename": { "type": "string"},
+        "url": { "type": "string", "format": "uri"},
+        "mimetype": {"type": "string"},
+        "date": {"type": "string", "format": "date-time"},
+        "type": {"type": "string"},
+        "width": {"type": "number"},
+        "height": {"type": "number"}
+      },
+      "required": ["url", "mimetype"]
+    },
+    "video": {
+      "title" : "Video",
+      "type": "object",
+      "properties": {
+        "filename": { "type": "string"},
+        "url": { "type": "string", "format": "uri"},
+        "videoId": {"type": "number"},
+        "mimetype": {"type": "string"},
+        "date": {"type": "string", "format": "date-time"},
+        "type": {"type": "string"},
+        "width": {"type": "number"},
+        "height": {"type": "number"}
+      },
+      "required": ["videoId", "mimetype"]
+    },
+    "award": {
+      "title" : "Award",
+      "description": "Inline SVG for award shown in the recognition section.",
+      "type": "string",
+      "pattern": "^<svg.+</svg>$"
+    },
+  }
+}
